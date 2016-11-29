864bae4a5fc4b25e16d242ee68bd05c5364a7d6e
Stephanie Gurung <stephr3@gmail.com>
2016-11-28 20:52:55 -0800

    allow filter queries for films get requests

--- films/views.py
+++ films/views.py
@@ -2,10 +2,27 @@ from films.models import *
 from films.serializers import *
 from rest_framework.response import Response
 from rest_framework import generics
+import pdb
 
 class FilmList(generics.ListCreateAPIView):
     queryset = Film.objects.all()
     serializer_class = FilmSerializer
+
+    def get(self, request, *args, **kwargs):
+        first_letter = request.GET.get('first_letter', '')
+        year_prod = request.GET.get('year_prod', '')
+        if first_letter:
+            if year_prod:
+                films = Film.objects.filter(title__istartswith=first_letter).filter(year_prod=year_prod)
+            else:
+                films = Film.objects.filter(title__istartswith=first_letter)
+        elif year_prod:
+            films = Film.objects.filter(year_prod=year_prod)
+        else:
+            films = Film.objects.all()
+
+        serialized_films = FilmSerializer(films, many=True)
+        return Response(serialized_films.data)
 
 class FilmDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Film.objects.all()
